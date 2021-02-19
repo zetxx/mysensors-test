a6b81ca40be259d1e8f1b3f0b81b926bb013729a
zetxx <zetxx@belogradchik.biz>
2021-02-20 00:22:49 +0200

    fix: init

---
+++ DHT/DHT/DHT.ino
@@ -0,0 +1,108 @@
+// board
+// tools > board > arch liux arduino avr board > arduino pro or pro mini
+// processor Atmega 328P (3.3V 8Mhz)
+#define MY_DEBUG
+#define MY_RADIO_RF24
+#define MY_NODE_ID 101
+
+#include <SPI.h>
+#include <MySensors.h>
+// DHT Temperature & Humidity Sensor
+// Unified Sensor Library Example
+// Written by Tony DiCola for Adafruit Industries
+// Released under an MIT license.
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 3     // Digital pin connected to the DHT sensor 
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+#define CHILD_ID_HUM 0
+#define CHILD_ID_TEMP 1
+
+uint32_t delayMS;
+static const uint64_t UPDATE_INTERVAL = 60000;
+
+MyMessage msgHum(CHILD_ID_HUM, V_HUM);
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+
+void setup() {
+  // Initialize device.
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+
+void presentation() {
+  // Send the sketch version information to the gateway
+  sendSketchInfo("TemperatureAndHumidity", "1.1");
+
+  // Register all sensors to gw (they will be created as child devices)
+  present(CHILD_ID_HUM, S_HUM);
+  present(CHILD_ID_TEMP, S_TEMP);
+}
+
+
+void loop() {
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  } else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+    send(msgTemp.set(event.temperature, 1));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  } else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+    send(msgHum.set(event.relative_humidity, 1));
+  }
+  sleep(UPDATE_INTERVAL);
+}
---
+++ mysensor-GW-esp32/gw/gw.ino
@@ -0,0 +1,98 @@
+/*
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2019 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/MySensors/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - tekka
+ *
+ * DESCRIPTION
+ * The ESP32 gateway sends data received from sensors to the WiFi link.
+ * The gateway also accepts input on ethernet interface, which is then sent out to the radio network.
+ * https://github.com/mysensors/MySensors/blob/development/hal/architecture/ESP32/MyHwESP32.h
+ * https://github.com/espressif/arduino-esp32/blob/master/variants/feather_esp32/pins_arduino.h
+ * https://forums.adafruit.com/viewtopic.php?f=24&t=140279
+ * https://www.mysensors.org/build/connect_radio
+ * Make sure to fill in your ssid and WiFi password below.
+ * | IO            | RF24 |
+ * |-------------|---------|
+ * | MOSI       | 18      |
+ * | MISO       | 19      |
+ * | SCK        | 5        |
+ * | CSN(SS) | 33      |
+ * | CE          | 15      |
+ */
+// tools > board > esp32 arduino > Adafruit esp32 feather
+// Enable debug prints to serial monitor
+#define MY_DEBUG
+
+// Enables and select radio type (if attached)
+#define MY_RADIO_RF24
+#define MY_RF24_CE_PIN 15
+
+#define MY_GATEWAY_MQTT_CLIENT
+#define MY_GATEWAY_ESP32
+
+// Set this node's subscribe and publish topic prefix
+#define MY_MQTT_PUBLISH_TOPIC_PREFIX "sofia-gateway1-out"
+#define MY_MQTT_SUBSCRIBE_TOPIC_PREFIX "sofia-gateway1-in"
+
+// Set MQTT client id
+#define MY_MQTT_CLIENT_ID "mysensors-1"
+
+// Enable these if your MQTT broker requires usenrame/password
+//#define MY_MQTT_USER "username"
+//#define MY_MQTT_PASSWORD "password"
+
+// Set WIFI SSID and password
+#define MY_WIFI_SSID "ssid"
+#define MY_WIFI_PASSWORD "passwd"
+
+// Set the hostname for the WiFi Client. This is the hostname
+// passed to the DHCP server if not static.
+#define MY_HOSTNAME "ESP32_MQTT_GW"
+
+// Enable MY_IP_ADDRESS here if you want a static ip address (no DHCP)
+//#define MY_IP_ADDRESS 192,168,178,87
+
+// If using static ip you can define Gateway and Subnet address as well
+//#define MY_IP_GATEWAY_ADDRESS 192,168,178,1
+//#define MY_IP_SUBNET_ADDRESS 255,255,255,0
+
+// MQTT broker ip address.
+#define MY_CONTROLLER_IP_ADDRESS 10,100,101,81
+
+// The MQTT broker port to to open
+#define MY_PORT 1883
+
+#include <MySensors.h>
+
+void setup()
+{
+  // Setup locally attached sensors
+}
+
+void presentation()
+{
+  // Present locally attached sensors here
+}
+
+void loop()
+{
+  // Send locally attech sensors data here
+}
